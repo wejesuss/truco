8bdf2851c8b690a807981a6fedcab27735ee5e6b
Wemerson Jesus <wejesuss@gmail.com>
2022-09-26 23:05:11 -0300

    feat: show and calculate available cards for a given player

--- match.c
+++ match.c
@@ -15,6 +15,8 @@ typedef struct trick
   bool is_tied_by_user;
 } trick;
 
+trick play_first_trick(player *user_ptr, player *cpu_ptr);
+
 void play_hand(card *cards, player *user_ptr, player *cpu_ptr)
 {
   int *user_tentos = (*user_ptr).player_tentos;
@@ -31,10 +33,49 @@ void play_hand(card *cards, player *user_ptr, player *cpu_ptr)
     draw_cards(cards, user_ptr, cpu_ptr);
   }
 
+  trick tricks[3];
+  tricks[0] = play_first_trick(user_ptr, cpu_ptr);
+  // show player cards
+  // ask for a card
+  // verify results
+  // store on tricks
+
+  // show player cards
+  // ask for a card to previous winner (or "cangador")
+  // verify results
+  // store on tricks
+
   // cards should be made available again here
   reset_deck(cards);
   // toggling who will shuffle the cards next
   is_user_foot = !is_user_foot;
 
   (*user_tentos) += 2;
+}
+
+int show_player_cards(card *player_cards)
+{
+  char cardname[15];
+  int available = 0;
+  for (size_t i = 0; i < TOTAL_HAND_CARDS_NUMBER; i++)
+  {
+    card card = player_cards[i];
+
+    if (card.available)
+    {
+      available++;
+      printf("%s ", get_card_name(&cardname[i * 5], card.suit, card.rank));
+    }
+  }
+
+  printf("\n");
+
+  return available;
+}
+
+trick play_first_trick(player *user_ptr, player *cpu_ptr)
+{
+  trick first_trick;
+
+  return first_trick;
 }
